6fc7fa0fd54ac7de37aa0e6e9b4dd20d4cee91d0
Dan Pock <dpock32509@gmail.com>
2016-12-22 20:03:02 -0500

    ?

--- .travis.sh
+++ .travis.sh
@@ -8,4 +8,4 @@ cd php-ast-master/
 phpize
 ./configure
 make
-sudo make install && echo "extension=<extension>.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+make install && echo "extension=<extension>.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
