a7f808b02d532e7673e77f10e01a74dc12008800
Dan Pock <dpock32509@gmail.com>
2016-12-24 05:59:28 -0500

    updates for phan

--- .phan/config.php
+++ .phan/config.php
@@ -1,16 +1,19 @@
 <?php
 
+use \Phan\Config;
+
 /**
  * This configuration will be read and overlaid on top of the
  * default configuration. Command line arguments will be applied
  * after this file is read.
  */
 return [
+    "allow_missing_properties" => false,
     'quick_mode' => false,
     'processes' => 2,
     'analyze_signature_compatibility' => true,
     'dead_code_detection' => true,
-    'minimum_severity' => Issue::SEVERITY_CRITICAL,
+    'minimum_severity' => 0,
     'directory_list' => [
         'src',
         'vendor/league/uri',
