f80e563f6e09f404666297ea3f573f96f195a05a
Dan Pock <dpock@liquidweb.com>
2017-01-30 11:41:20 -0500

    Use trait for new exception domain tracking
    (cherry picked from commit a3dc111a7ae2126d6dff721db1f06327130aeb80)
    
    # Conflicts:
    #	src/Exceptions/CouldNotDownloadCertificate.php
    #	src/Exceptions/InvalidUrl.php

--- src/Exceptions/CouldNotDownloadCertificate.php
+++ src/Exceptions/CouldNotDownloadCertificate.php
@@ -7,26 +7,20 @@ use LiquidWeb\SslCertificate\Url;
 
 class CouldNotDownloadCertificate extends Exception
 {
-    protected $errorDomain;
 
-    private function setErrorDomaain(string $domain)
-    {
-        $this->errorDomain = $domain;
-    }
+    use TrackDomainTrait;
 
     public static function hostDoesNotExist(string $hostName): CouldNotDownloadCertificate
     {
         $exception = new static("The host named `{$hostName}` does not exist.");
-        $exception->setErrorDomaain($hostName);
-
+        $exception->setErrorDomain($hostName);
         return $exception;
     }
 
     public static function noCertificateInstalled(string $hostName): CouldNotDownloadCertificate
     {
         $exception = new static("Could not find a certificate on  host named `{$hostName}`.");
-        $exception->setErrorDomaain($hostName);
-
+        $exception->setErrorDomain($hostName);
         return $exception;
     }
 
@@ -36,24 +30,21 @@ class CouldNotDownloadCertificate extends Exception
             return new static('Server does not support SSL over port 80.');
         }
         $exception = new static("Server SSL handshake error – the certificate for `{$url->getTestURL()}` will not work.");
-        $exception->setErrorDomaain($url->getHostName());
-
+        $exception->setErrorDomain($url->getHostName());
         return $exception;
     }
 
     public static function connectionTimeout(string $hostName): CouldNotDownloadCertificate
     {
         $exception = new static("Connection timed out while testing `{$hostName}`.");
-        $exception->setErrorDomaain($hostName);
-
+        $exception->setErrorDomain($hostName);
         return $exception;
     }
 
     public static function unknownError(string $hostName, string $errorMessage): CouldNotDownloadCertificate
     {
         $exception = new static("Could not download certificate for host `{$hostName}` because {$errorMessage}");
-        $exception->setErrorDomaain($hostName);
-
+        $exception->setErrorDomain($hostName);
         return $exception;
     }
 }
---
+++ src/Exceptions/TrackDomainTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LiquidWeb\SslCertificate\Exceptions;
+
+trait TrackDomainTrait {
+
+  protected $errorDomain;
+
+  private function setErrorDomain(string $domain) {
+    $this->errorDomain = $domain;
+  }
+
+  private function getErrorDomain(string $domain) {
+     return $this->errorDomain;
+  }
+
+}
