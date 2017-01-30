121caf2caa15ab82cf8bc879b67d1c565f5c670c
Dan <dpock32509@gmail.com>
2017-01-30 16:32:06 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidUrl.php
+++ src/Exceptions/InvalidUrl.php
@@ -6,30 +6,34 @@ use Exception;
 
 class InvalidUrl extends Exception
 {
-  protected $errorDomain;
+    protected $errorDomain;
 
-  private function setErrorDomaain(string $domain) {
-    $this->errorDomain = $domain;
-  }
+    private function setErrorDomaain(string $domain)
+    {
+        $this->errorDomain = $domain;
+    }
 
-  public static function couldNotValidate(string $url): InvalidUrl
-  {
-      $exception = new static("String `{$url}` is not a valid url.");
-      $exception->setErrorDomaain('google.com');
-      return $exception;
-  }
+    public static function couldNotValidate(string $url): InvalidUrl
+    {
+        $exception = new static("String `{$url}` is not a valid url.");
+        $exception->setErrorDomaain('google.com');
 
-  public static function couldNotDetermineHost(string $url): InvalidUrl
-  {
-      $exception = new static("Could not determine host from url `{$url}`.");
-      $exception->setErrorDomaain('google.com');
-      return $exception;
-  }
+        return $exception;
+    }
 
-  public static function couldNotResolveDns(string $hostName): InvalidUrl
-  {
-      $exception = new static("The domain `{$hostName}` does not have a valid DNS record.");
-      $exception->setErrorDomaain($hostName);
-      return $exception;
-  }
+    public static function couldNotDetermineHost(string $url): InvalidUrl
+    {
+        $exception = new static("Could not determine host from url `{$url}`.");
+        $exception->setErrorDomaain('google.com');
+
+        return $exception;
+    }
+
+    public static function couldNotResolveDns(string $hostName): InvalidUrl
+    {
+        $exception = new static("The domain `{$hostName}` does not have a valid DNS record.");
+        $exception->setErrorDomaain($hostName);
+
+        return $exception;
+    }
 }
